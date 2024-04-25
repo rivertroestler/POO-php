14777845e670ac9d5e6c61348ecf72b40cecd795
rivertroestler <167063264+rivertroestler@users.noreply.github.com>
2024-04-25 11:37:34 +0200

    exo5.php

--- exo5.php
+++ exo5.php
@@ -1,41 +1,46 @@
-<h1>Exercice 15</h1>
+<h1>Exercice 4</h1>
 
-<p>Créer une classe Voiture possédant 2 propriétés (marque et modèle) ainsi qu’une classe VoitureElec 
-qui hérite (extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie)<br> 
-Instancier une voiture « classique » et une voiture « électrique » ayant les caractéristiques 
-suivantes :<br>
-Peugeot 408 : $v1 = new Voiture("Peugeot","408");<br>
-BMW i3 150 : $ve1 = new VoitureElec("BMW","I3",100);
-<br>
-Votre programme de test devra afficher les informations des 2 voitures de la façon suivante : <br><br>
-echo $v1->getInfos()."<br/>"; <br>
-echo $ve1->getInfos()."<br/>";<br>
+<p>Créer une classe Voiture possédant les propriétés suivantes : marque, modèle, nbPortes et 
+vitesseActuelle ainsi que les méthodes demarrer( ), accelerer( ) et stopper( ) en plus 
+des accesseurs (get) et mutateurs (set) traditionnels. La vitesse initiale de chaque véhicule 
+instancié est de 0. Une méthode personnalisée displayInfos() pourra afficher toutes les informations 
+d’un véhicule. <br> 
+v1 ➔ "Peugeot","408",5 <br>
+v2 ➔ "Citroën","C4",3 <br> <br>
+Coder l’ensemble des méthodes, accesseurs et mutateurs de la classe tout en réalisant des jeux de 
+tests pour vérifier la cohérence de la classe Voiture. Vous devez afficher les tests et les éléments 
+suivants :
+
 
 </p>
 
 <h2>Résultat</h2>
 
 <?php
-//Classe mère de VoitureElec
 
-class Voiture{
+class Voiture {
 
-    protected string $_marque;
-    protected string $_modele;
-
-    public function __construct(string $_marque, string $_modele) {
+    private string $_marque;
+    private string $_modele;
+    private int $_nbPortes;
+    private int $_vitesseActuelle = 0;
+    private bool $_statut = false;
+    
+    public function __construct(string $_marque, string $_modele, int $_nbPortes) {
 
         $this->_marque = $_marque;
         $this->_modele = $_modele;
+        $this->_nbPortes = $_nbPortes;
     }
     
-    //_marque 
-    public function get_marque(): string
+
+ //_marque 
+    public function get_marque()
     {
         return $this->_marque;
     }
-
-    public function set_marque(string $_marque)
+ 
+    public function set_marque($_marque)
     {
         $this->_marque = $_marque;
 
@@ -43,67 +48,146 @@ class Voiture{
     }
 
     //_modele
-    public function get__modele(): string
+    public function get__modele()
     {
         return $this->_modele;
     }
 
-    public function set__modele(string $_modele)
+    public function set__modele($_modele)
     {
         $this->_modele = $_modele;
 
         return $this;
     }
 
-/*---------méthodes publiques-Classe Voiture--------*/ 
-public function getInfos(){
-    return $this->_marque." ".$this->_modele;
-}
 
-
-
-public function __toString() {
-    return $this->_marque." ".$this->_modele;
-}
-}
-
-
-
-//Classe fille de Voiture
-Class VoitureElec extends Voiture{
-
-    private int $_autonomie;
-
-    public function __construct(string $_marque, string $_modele, int $_autonomie)
+    //_nbPortes
+    public function get_nbPortes()
     {
-        parent::__construct($_marque, $_modele);
-        $this->_autonomie = $_autonomie;
+        return $this->_nbPortes;
     }
 
-    public function get_autonomie(): int
+    public function set_nbPortes($_nbPortes)
     {
-        return $this->_autonomie;
-    }
-
-    public function set_autonomie(int $_autonomie)
-    {
-        $this->_autonomie = $_autonomie;
+        $this->_nbPortes = $_nbPortes;
 
         return $this;
     }
 
-    public function getInfos(){
-        return parent::__toString()." ".$this->_autonomie;
+   
+    //_vitesseActuelle
+    public function get_vitesseActuelle()
+    {
+        return $this->_vitesseActuelle;
+    }
+
+    public function set_vitesseActuelle($_vitesseActuelle)
+    {
+        $this->_vitesseActuelle = $_vitesseActuelle;
+
+        return $this;
+    }
+
+
+    //méthode démarrer(_vitesseActuelle)
+
+    public function demarrer() {
+        if($this->_statut == true) {
+            return "Le véhicule ".$this->_marque." est démarré <br>Sa vitesse actuelle est de: ".$this->_vitesseActuelle." km/h<br>";
+        }
+        else {
+            $this->_statut = true;
+            return "Le véhicule ".$this->_marque." ".$this->_modele." démarre<br>";
+        }
+    }
+
+
+    //methode accélérer($_vitesseActuelle)
+
+    public function accelerer(int $_vitesseActuelle) {
+
+        if($this->_statut == false) {
+            return "Le véhicule ".$this->_marque." ".$this->_modele." veut accélérer de ".$_vitesseActuelle."<br>Pour accélérer, il faut démarrer le véhicule ".$this->_marque." ".$this->_modele."<br>";
+        }
+        else {
+            $this->_vitesseActuelle += $_vitesseActuelle;
+            return "Le véhicule ".$this->_marque." ".$this->_modele." accélère de ".$this->_vitesseActuelle."km/h<br>";
+        }
+    }
+
+    //méthode ralentir
+
+    public function ralentir(int $_vitesseActuelle) {
+        if ($this->_vitesseActuelle > $_vitesseActuelle) {
+            $this->_vitesseActuelle -= $_vitesseActuelle;
+            return "Le véhicule ".$this->_marque." ".$this->_modele." ralentit de ".$_vitesseActuelle."km/h<br>";
+        }
+        elseif ($this->_statut == false) {
+            return "Le véhicule ".$this->_marque." ".$this->_modele." est déjà à l'arrêt <br>";
+        }
+        else {
+            $this->_vitesseActuelle = 0;
+            return "Le véhicule ".$this->_marque." ".$this->_modele." est à l'arrêt <br>";
+        }
+    }
+
+    //méthode stopper(_vitesseActuelle)
+
+    public function stopper() {
+        if($this->_statut == false ) {
+            return "Le véhicule ".$this->_marque." est à l'arrêt <br>Sa vitesse actuelle est de: ".$this->_vitesseActuelle." km/h<br>";
+        }
+        else {
+            $this->_statut = false;
+            $this->_vitesseActuelle = 0;
+            return "Le véhicule ".$this->_marque." ".$this->_modele." est stoppé<br>";
+        }
+    }
+
+    //méthode displayInfos()
+
+    public function displayInfos($infos): string {
+        switch ($infos) {
+            case "nom":
+                return "Nom et modèle du véhicule: ".$this->_marque." ".$this->_modele."<br> Nombre de portes: ".$this->_nbPortes."<br>";
+                break;
+            case "vitesse":
+                return "La vitesse du véhicule ".$this->_marque." ".$this->_modele." est de ".$this->_vitesseActuelle." km/h<br>";
+                break;
+        }
+    }
+
+
+    public function __toString() {
+        return $this->_marque." ".$this->_modele;
     }
 }
+//**********Objets classe Voiture()**********/
+$v1 = new Voiture("Peugeot","408",5);
+$v2 = new Voiture("Citroën","C4",3);
 
 
-/*---------variables---------*/ 
+//**********Tests**********//
+//bloc 1
 
-$v1 = new Voiture("Peugeot","408");
-$ve1 = new VoitureElec("BMW","I3",100);
+echo $v1->demarrer();
+echo $v1->accelerer(50);
+echo $v2->demarrer();
+echo $v2->stopper();
+echo $v2->accelerer(20);
+echo $v1->displayInfos("vitesse");
+echo $v2->displayInfos("vitesse");
 
+//bloc 2
+echo $v1->displayInfos("nom");
+echo $v1->demarrer();
 
-/*---------test---------*/ 
-echo $v1->getInfos()."<br/>";
-echo $ve1->getInfos()."<br/>";
+//bloc 3
+echo $v2->displayInfos("nom");
+echo $v2->stopper();
+
+// ralentir() tests
+echo "<br><br><br><br>";
+echo $v1->ralentir(10);
+echo $v2->ralentir(10);
+echo $v1->ralentir(60);
