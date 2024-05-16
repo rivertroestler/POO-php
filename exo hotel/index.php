98e0ee0dec0e061fd972444c1cd94645c407b539
rivertroestler <167063264+rivertroestler@users.noreply.github.com>
2024-05-16 10:37:11 +0200

    Update index.php

--- exo hotel/index.php
+++ exo hotel/index.php
@@ -16,28 +16,38 @@
 
     $client1 = new Client("Virgile", "GIBELLO");
     $client2 = new Client("Micka", "MURMAN");
-
-    $h1 = new Hotel("Hilton ****", "Strasbourg", "10 route de la Gare 67000", 17);
-    $h2 = new Hotel("Regent ****", "Paris", "22 rue du Debarcadere 75017", 5);
+//(donner un nom compréhensible à une variable)
+    $h1 = new Hotel("Hilton ****", "Strasbourg", "10 route de la Gare 67000", 30);
+    $h2 = new Hotel("Regent ****", "Paris", "22 rue du Debarcadere 75000", 5);
 
     $chambre1 = new Chambre(1, 2, 120, $h1);
     $chambre2 = new Chambre(2, 2, 120, $h1);
-    $chambre3 = new Chambre(3, 2, 100, $h1);
-    $chambre4 = new Chambre(4, 2, 200, $h1);
-    $chambre5 = new Chambre(5, 16, 500, $h1);
+    $chambre3 = new Chambre(3, 2, 120, $h1);
+    $chambre4 = new Chambre(4, 2, 120, $h1);
     $chambre17 = new Chambre(17, 1, 300, $h1);
 
-    $r1 = new Reservation($h1, $chambre17, $client2, "01-01-2021", "01-01-2021");
-    $r2 = new Reservation($h1, $chambre17, $client1, "11-03-2021", "11-03-2021");
-    $r3 = new Reservation($h1, $chambre3, $client1, "01-04-2021", "01-04-2021");
-    $r4 = new Reservation($h1, $chambre5, $client1, "10-10-2022", "03-11-2022");
+    $r1 = new Reservation($h1, $chambre3, $client2, "11-03-2021", "11-03-2021");
+    $r2 = new Reservation($h1, $chambre17, $client1, "01-01-2021", "01-01-2021");
+    $r3 = new Reservation($h1, $chambre4, $client2, "01-04-2021", "01-04-2021");
+
     echo $h1;
     echo $h1->afficherReservation();
     echo $h2->afficherReservation();
     echo $client1->afficherReservation();
     echo $client2->afficherReservation();
     echo $h1->afficherStatutChambre();
-    
+
+//classe: une classe est composée d'attributs(variables) et de méthodes(comportements des attributs) spécifiques. Les objets d'une même classe partagent ces 'paramètres'
+
+//objet: l'objet est l'instance d'une classe, il a en commun avec sa classe, le type de ses attributs et peut utiliser les méthodes de sa classe
+
+//chainage: plusieurs méthodes peuvent être utilisées sur un même objet en une seule instruction
+
+//principe d'encapsulation: l'encapsulation permet de protéger les objets en empêchant l'accès direct aux données d'un objet (private, protected et public)
+//portée d'une variable: la portée d'une variable mesure la partie du code où cette variable est accesible
+//différence include require , include affiche une erreur si un fichier est introuvable mais le code continue de s'executer, alors que require affiche une erreur fatale et stoppe le programme
+
+  ?>  
 </body>
 
 </html>
